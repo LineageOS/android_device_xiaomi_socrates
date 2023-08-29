9e2a0701e022c44ef77034be6361944c09a64c6d
Aaron Kling <webgeek1234@gmail.com>
2023-08-29 15:58:31 -0500

    socrates: Add vendor soong imports to extract
    Change-Id: Ib19e69dc35af6b04519eeb695228af65f3d01d2a

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -25,6 +25,17 @@ if [ ! -f "${HELPER}" ]; then
 fi
 source "${HELPER}"
 
+function vendor_imports() {
+    cat <<EOF >>"$1"
+		"device/xiaomi/socrates",
+		"hardware/qcom-caf/wlan",
+		"hardware/qcom-caf/sm8550",
+		"hardware/xiaomi",
+		"vendor/qcom/opensource/commonsys-intf/display",
+		"vendor/qcom/opensource/dataservices",
+EOF
+}
+
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
 
