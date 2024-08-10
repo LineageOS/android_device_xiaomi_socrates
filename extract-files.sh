cc0a24f0eb4bc87c2a922a618cf125d27f59e63f
danielml <daniel@danielml.dev>
2024-08-10 19:52:31 +0200

    socrates: Initial sorted proprietary files list
    Base proprietary files list taken from
    LineageOS/android_device_oneplus_sm8550-common.
    
    This change also gets rid of 32 bit libraries where possible, and
    removes unneeded services and libraries.
    
    Change-Id: Iac728138eba2aaf2cf5fc148d4bfd2e4b1a853f2

--- extract-files.sh
+++ extract-files.sh
@@ -88,7 +88,6 @@ setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
 if [ -z "${ONLY_FIRMWARE}" ]; then
     extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
-    extract "${MY_DIR}/proprietary-files-vendor.txt" "${SRC}" "${KANG}" --section "${SECTION}"
 fi
 
 if [ -z "${SECTION}" ]; then
