f50d0dcdc12c6f646e9b64819313d3feecc82622
Cosmin Tanislav <demonsingur@gmail.com>
2024-08-18 09:52:31 +0300

    socrates: Skip dependencies which aren't defined in .bp
    Change-Id: I01a653661856495788c05b720c0577dac44998a4

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -47,6 +47,22 @@ function lib_to_package_fixup_vendor_variants() {
         vendor.qti.diaghal@1.0)
             echo "$1-vendor"
             ;;
+        libagmclient | \
+        libpalclient | \
+        libwpa_client) ;;
+        *)
+            return 1
+            ;;
+    esac
+}
+
+function lib_to_package_fixup_odm_variants() {
+    if [ "$2" != "odm" ]; then
+        return 1
+    fi
+
+    case "$1" in
+        libagmmixer) ;;
         *)
             return 1
             ;;
@@ -56,7 +72,8 @@ function lib_to_package_fixup_vendor_variants() {
 function lib_to_package_fixup() {
     lib_to_package_fixup_clang_rt_ubsan_standalone "$1" ||
         lib_to_package_fixup_proto_3_9_1 "$1" ||
-        lib_to_package_fixup_vendor_variants "$@"
+        lib_to_package_fixup_vendor_variants "$@" ||
+        lib_to_package_fixup_odm_variants "$@"
 }
 
 # Initialize the helper
