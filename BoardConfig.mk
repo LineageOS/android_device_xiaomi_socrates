2a047f99a3a2721ce3477af9d5c90f7fbbbd3fbf
danielml <daniel@danielml.dev>
2024-09-17 10:25:14 +0200

    socrates: Unset BUILD_BROKEN flags
    Change-Id: I86a5fdfcc8f656ec0dfd1c38b4ad1f87ebd7f556
    Signed-off-by: danielml <daniel@danielml.dev>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,9 +6,6 @@
 
 DEVICE_PATH := device/xiaomi/socrates
 
-BUILD_BROKEN_DUP_RULES := true
-BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
-
 # A/B
 AB_OTA_UPDATER := true
 AB_OTA_PARTITIONS := \
