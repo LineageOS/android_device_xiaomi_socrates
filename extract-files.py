07ef6a20cdfa3e591cf19d035bba7264858fe853
LuK1337 <priv.luk@gmail.com>
2024-11-06 11:56:59 +0100

    socrates: Use common lib_fixups
    Change-Id: I862e547f3375184729eb1a909bb7d6f676d45dfe

--- extract-files.py
+++ extract-files.py
@@ -12,9 +12,8 @@ from extract_utils.fixups_blob import (
     blob_fixups_user_type,
 )
 from extract_utils.fixups_lib import (
-    lib_fixup_vendorcompat,
+    lib_fixups,
     lib_fixups_user_type,
-    libs_proto_3_9_1,
 )
 from extract_utils.main import (
     ExtractUtils,
@@ -30,7 +29,7 @@ namespace_imports = [
 ]
 
 lib_fixups: lib_fixups_user_type = {
-    libs_proto_3_9_1: lib_fixup_vendorcompat,
+    **lib_fixups,
 }
 
 blob_fixups: blob_fixups_user_type = {
