eeea37976fbaac4829e94e0d62af661b17cb7142
danielml <daniel@danielml.dev>
2024-11-27 22:03:36 +0100

    socrates: Update from OS2.0.3.0.VMKCNXM
    - Update VINTF configurations
    - Remove unneeded blob fixups
    - Add required selinux rules
    
    Change-Id: Ifc2790a76e7377c2cd33d73fba998118b04340b8
    Signed-off-by: danielml <daniel@danielml.dev>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -195,11 +195,14 @@ TARGET_QTI_VIBRATOR_USE_EFFECT_STREAM := true
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/configs/vintf/framework_matrix.xml \
+    $(DEVICE_PATH)/configs/vintf/product_framework_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
-DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/vintf/manifest.xml
+DEVICE_MANIFEST_FILE := \
+    $(DEVICE_PATH)/configs/vintf/manifest_kalama.xml \
+    $(DEVICE_PATH)/configs/vintf/manifest_socrates.xml
 
 # Vendor security patch
-VENDOR_SECURITY_PATCH := 2024-08-01
+VENDOR_SECURITY_PATCH := 2024-12-01
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
