4afe1478a23e9fccbeb144841dcc9929ed3ff8a5
Marc K <morckx@gmail.com>
2024-12-22 11:50:28 +0100

    socrates: overlay: Enable proximity check on wake
    Change-Id: I6506408447b15464238a2eefd98306efbaffbd84

--- device.mk
+++ device.mk
@@ -239,6 +239,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     CarrierConfigOverlaySocrates \
     FrameworkResOverlaySocrates \
+    LineageSdkOverlaySocrates \
     SystemUIOverlaySocrates \
     SettingsOverlaySocrates \
     TelephonyOverlaySocrates
