2e9d1824fd3251d81b81d823ac5a6889ac8d9b73
lolipuru <banstellazz@gmail.com>
2025-06-16 17:22:15 +0800

    socrates: Commonized mixer path and resourcemanager
    mixer path from AUDIO.LA.8.0.r1-12300-KAILUA.0
    resourcemanager from OS2.0.200.7.VMCCNXM
    
    Change-Id: Id70fe71d9d3e76f0df90f2086dc4f4e0d2ec8e6d
    Signed-off-by: lolipuru <banstellazz@gmail.com>

--- device.mk
+++ device.mk
@@ -10,6 +10,11 @@ $(call inherit-product, device/xiaomi/sm8550-common/common.mk)
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/xiaomi/socrates/socrates-vendor.mk)
 
+# AudioAdd commentMore actions
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/audio/mixer_paths_kalama_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_kalama/mixer_paths_kalama_mtp.xml \
+    $(LOCAL_PATH)/configs/audio/resourcemanager_kalama_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_kalama/resourcemanager_kalama_mtp.xml
+
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/goodix_ts.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/goodix_ts.kl
