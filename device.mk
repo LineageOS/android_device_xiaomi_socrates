8cc8db080d45c33608182626a4fbf646218335fc
WenHao2130 <wenhao2130@outlook.com>
2025-06-24 16:05:53 +0800

    socrates: Fix typo error in device.mk
    Copy patch from GitHub without deleting excess text
    
    CRs-Fixed: 435090
    Change-Id: I4bd2243a16b1f0ae126f809f7d03a4a57bec9019
    Signed-off-by: WenHao2130 <wenhao2130@outlook.com>

--- device.mk
+++ device.mk
@@ -10,7 +10,7 @@ $(call inherit-product, device/xiaomi/sm8550-common/common.mk)
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/xiaomi/socrates/socrates-vendor.mk)
 
-# AudioAdd commentMore actions
+# Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/mixer_paths_kalama_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_kalama/mixer_paths_kalama_mtp.xml \
     $(LOCAL_PATH)/configs/audio/resourcemanager_kalama_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_kalama/resourcemanager_kalama_mtp.xml
