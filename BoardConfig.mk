b4b06ab1c84e3226b21163c3582155c79839a6af
Arian <arian.kulmer@web.de>
2025-06-26 21:02:17 +0800

    socrates: Enable single_tap sensor and set permissions for touch sensors
    Change-Id: I777ed71ecc5dc502182567bc7e1166605251af93

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,6 +28,9 @@ BOOT_KERNEL_MODULES += \
 # Power
 TARGET_TAP_TO_WAKE_NODE := /sys/devices/platform/goodix_ts.0/double_tap_enable
 
+# Sepolicy
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+
 # System properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
--- device.mk
+++ device.mk
@@ -15,6 +15,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/mixer_paths_kalama_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_kalama/mixer_paths_kalama_mtp.xml \
     $(LOCAL_PATH)/configs/audio/resourcemanager_kalama_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_kalama/resourcemanager_kalama_mtp.xml
 
+# Init
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/init/init.socrates.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.socrates.rc
+
 # Overlay
 PRODUCT_PACKAGES += \
     FrameworkResOverlaySocrates \
