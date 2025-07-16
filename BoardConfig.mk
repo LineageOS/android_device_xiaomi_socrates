4dd03facbb5e07052bd161868ec8395598b9c9e9
WenHao2130 <wenhao2130@outlook.com>
2025-07-16 18:52:03 +0800

    Revert "socrates: Fix fast charging"
    This reverts commit 4c647cb1523e5f642c96696542f302e12821879b.
    
    This file_contexts has been commonized
    
    Change-Id: I82a704dfbb6f1bfd71bce6de902ec7c5f8abf504
    Signed-off-by: WenHao2130 <wenhao2130@outlook.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,9 +32,6 @@ TARGET_TAP_TO_WAKE_NODE := /sys/devices/platform/goodix_ts.0/double_tap_enable
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
-# Sepolicy
-BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
-
 # Vibrator
 TARGET_QTI_VIBRATOR_EFFECT_LIB := libqtivibratoreffect.xiaomi
 TARGET_QTI_VIBRATOR_USE_EFFECT_STREAM := true
