1d26e7b4b00faa54f51eafc28e0cdcf9575318d2
WenHao2130 <wenhao2130@outlook.com>
2025-10-21 13:19:29 +0800

    Revert "socrates: Add keylayout for wakeup keys"
    This reverts commit a5e6e4548ceef48c3769e86bafd5a40735ee1cf4.
    
    dt2w has been implemented by sensors.xiaomi.v2 and is no longer useful
    
    Change-Id: I9880d1aacd084652049ff14e4e650035dee3b34c
    Signed-off-by: WenHao2130 <wenhao2130@outlook.com>

--- device.mk
+++ device.mk
@@ -15,10 +15,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/mixer_paths_kalama_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_kalama/mixer_paths_kalama_mtp.xml \
     $(LOCAL_PATH)/configs/audio/resourcemanager_kalama_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_kalama/resourcemanager_kalama_mtp.xml
 
-# Keylayout
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/keylayout/goodix_ts.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/goodix_ts.kl
-
 # Overlay
 PRODUCT_PACKAGES += \
     FrameworkResOverlaySocrates \
