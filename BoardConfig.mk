cf78895728984badbb6851545ac1fb381ed1ca17
sb6596 <shubhamprince111@gmail.com>
2025-11-27 19:58:27 +0800

    socrates: Drop unused TARGET_TAP_TO_WAKE_NODE flag
     * We now have sensors doing it for us
    
    Change-Id: Ia2f164f879bd895bba51b1d7db19efd09e03951a
    Signed-off-by: sb6596 <shubhamprince111@gmail.com>
    Signed-off-by: WenHao2130 <wenhao2130@outlook.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,9 +25,6 @@ BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD += \
 BOOT_KERNEL_MODULES += \
     goodix_core.ko
 
-# Power
-TARGET_TAP_TO_WAKE_NODE := /sys/devices/platform/goodix_ts.0/double_tap_enable
-
 # Sepolicy
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
