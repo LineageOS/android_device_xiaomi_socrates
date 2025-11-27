ddea623c471812d660eca05776ea06202aed690a
lolipuru <banstellazz@gmail.com>
2025-11-27 16:38:58 +0800

    socrates: Commonized xiaomi.sensors.v2
    Change-Id: I6d47307b52ebdc6270163a61db9a4bb7baed7818
    Signed-off-by: lolipuru <banstellazz@gmail.com>
    Signed-off-by: WenHao2130 <wenhao2130@outlook.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,9 +25,6 @@ BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD += \
 BOOT_KERNEL_MODULES += \
     goodix_core.ko
 
-# Sepolicy
-BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
-
 # System properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
