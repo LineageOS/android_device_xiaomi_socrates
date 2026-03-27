bdb6ae0d351208fd5881e6001c3ba9ffb7f08bb1
WenHao2130 <wenhao2130@outlook.com>
2026-03-27 22:43:42 +0800

    socrates: Update from OS3.0.5.0.VMKCNXM
    Change-Id: Ibaaef77b928ca560c12bdbc4c5ade4e10138c305
    Signed-off-by: WenHao2130 <wenhao2130@outlook.com>

--- lineage_socrates.mk
+++ lineage_socrates.mk
@@ -21,4 +21,4 @@ PRODUCT_BRAND := Redmi
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildFingerprint=Redmi/socrates/socrates:15/AQ3A.250226.002/OS3.0.4.0.VMKCNXM:user/release-keys
+    BuildFingerprint=Redmi/socrates/socrates:15/AQ3A.250226.002/OS3.0.5.0.VMKCNXM:user/release-keys
